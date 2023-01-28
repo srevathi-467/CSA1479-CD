97444b58e973b6a554cd263d0b6ea9bb1a69d38c
srevathi-467 <113590702+srevathi-467@users.noreply.github.com>
2023-01-28 17:19:14 +0530

    Add files via upload

---
+++ exp6a.c
@@ -0,0 +1,4 @@
+#include<stdio.h> void main()
+{
+int a,b,c = 30;
+ 	printf("hello");	
--- lex.yy.c
+++ lex.yy.c
@@ -284,27 +284,26 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 
 #define YY_NUM_RULES 4
 #define YY_END_OF_BUFFER 5
-static yyconst short int yy_accept[20] =
+static yyconst short int yy_accept[9] =
     {   0,
-        0,    0,    5,    3,    3,    0,    0,    0,    0,    0,
-        0,    0,    0,    0,    0,    1,    0,    2,    0
+        0,    0,    5,    2,    3,    1,    2,    0
     } ;
 
 static yyconst int yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    3,    1,    1,    1,    1,    1,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    4,    5,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
 
-        6,    7,    1,    1,    8,    1,    1,    9,    1,   10,
-        1,    1,    1,    1,    1,    1,   11,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -322,38 +321,29 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[12] =
+static yyconst int yy_meta[4] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1
+        1,    2,    2
     } ;
 
-static yyconst short int yy_base[21] =
+static yyconst short int yy_base[10] =
     {   0,
-        0,   17,   19,   21,    0,   12,    7,    9,   11,    6,
-        4,    2,    0,    3,    2,   21,    0,   21,   21,    0
+        0,    0,    5,    0,    6,    6,    0,    6,    3
     } ;
 
-static yyconst short int yy_def[21] =
+static yyconst short int yy_def[10] =
     {   0,
-       20,   20,   19,   19,   19,   19,   19,   19,   19,   19,
-       19,   19,   19,   19,   19,   19,   19,   19,    0,   19
+        8,    1,    8,    9,    8,    8,    9,    0,    8
     } ;
 
-static yyconst short int yy_nxt[33] =
+static yyconst short int yy_nxt[10] =
     {   0,
-        4,   19,   19,   19,    6,   18,   17,    7,   16,   19,
-       15,   14,   13,   12,   11,   10,    9,    8,   19,    5,
-        3,   19,   19,   19,   19,   19,   19,   19,   19,   19,
-       19,   19
+        4,    5,    6,    7,    8,    3,    8,    8,    8
     } ;
 
-static yyconst short int yy_chk[33] =
+static yyconst short int yy_chk[10] =
     {   0,
-       20,    0,    0,    0,    5,   17,   15,    5,   14,    0,
-       13,   12,   11,   10,    9,    8,    7,    6,    3,    2,
-       19,   19,   19,   19,   19,   19,   19,   19,   19,   19,
-       19,   19
+        1,    1,    1,    9,    3,    8,    8,    8,    8
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -367,11 +357,11 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "exp6.l"
+#line 1 "exp6a.l"
 #define INITIAL 0
-#line 2 "exp6.l"
-int nmacro, nheader;
-#line 375 "lex.yy.c"
+#line 2 "exp6a.l"
+int nchar, nword, nline;
+#line 365 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -514,9 +504,6 @@ YY_MALLOC_DECL
 #endif
 
 #define YY_RULE_SETUP \
-	if ( yyleng > 0 ) \
-		yy_current_buffer->yy_at_bol = \
-				(yytext[yyleng - 1] == '\n'); \
 	YY_USER_ACTION
 
 YY_DECL
@@ -525,9 +512,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 4 "exp6.l"
+#line 4 "exp6a.l"
 
-#line 531 "lex.yy.c"
+#line 518 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -566,7 +553,6 @@ YY_DECL
 		yy_bp = yy_cp;
 
 		yy_current_state = yy_start;
-		yy_current_state += YY_AT_BOL();
 yy_match:
 		do
 			{
@@ -579,13 +565,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 20 )
+				if ( yy_current_state >= 9 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 21 );
+		while ( yy_base[yy_current_state] != 6 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -613,25 +599,25 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 5 "exp6.l"
-{ nmacro++; }
+#line 5 "exp6a.l"
+{ nline++; nchar++; }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 6 "exp6.l"
-{ nheader++; }
+#line 6 "exp6a.l"
+{ nword++, nchar += yyleng; }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 7 "exp6.l"
-{ }
+#line 7 "exp6a.l"
+{ nchar++; }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 8 "exp6.l"
+#line 8 "exp6a.l"
 ECHO;
 	YY_BREAK
-#line 635 "lex.yy.c"
+#line 621 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -911,7 +897,6 @@ static yy_state_type yy_get_previous_state()
 	register char *yy_cp;
 
 	yy_current_state = yy_start;
-	yy_current_state += YY_AT_BOL();
 
 	for ( yy_cp = yytext_ptr + YY_MORE_ADJ; yy_cp < yy_c_buf_p; ++yy_cp )
 		{
@@ -924,7 +909,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 20 )
+			if ( yy_current_state >= 9 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -959,11 +944,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 20 )
+		if ( yy_current_state >= 9 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 19);
+	yy_is_jam = (yy_current_state == 8);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1082,7 +1067,6 @@ static int input()
 	*yy_c_buf_p = '\0';	/* preserve yytext */
 	yy_hold_char = *++yy_c_buf_p;
 
-	yy_current_buffer->yy_at_bol = (c == '\n');
 
 	return c;
 	}
@@ -1519,10 +1503,10 @@ int main()
 	return 0;
 	}
 #endif
-#line 8 "exp6.l"
+#line 8 "exp6a.l"
 
 int yywrap(void) { return 1;
 }
 int main(int argc, char *argv[]) { yyin = fopen(argv[1], "r"); yylex();
-printf("Number of macros defined = %d\n", nmacro); printf("Number of header files included = %d\n", nheader); fclose(yyin);
+printf("Number of characters = %d\n", nchar); printf("Number of words = %d\n", nword); printf("Number of lines = %d\n", nline); fclose(yyin);
 }
